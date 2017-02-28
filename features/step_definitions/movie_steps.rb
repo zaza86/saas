ce8f7a7cae700eeb2ff115c400bf59b58cc6b454
zaza86 <fieza.rosza@gmail.com>
2017-02-28 12:23:54 -0600

    Create movie_steps.rb

---
+++ features/step_definitions/movie_steps.rb
@@ -0,0 +1,50 @@
+# Add a declarative step here for populating the DB with movies.
+
+Given /the following movies exist/ do |movies_table|
+  movies_table.hashes.each do |movie|
+    Movie.create!(movie)
+    # each returned element will be a hash whose key is the table header.
+    # you should arrange to add that movie to the database here.
+  end
+  # fail "Unimplemented"
+end
+
+# Make sure that one string (regexp) occurs before or after another one
+#   on the same page
+
+Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
+  if page.body.index(e1) > page.body.index(e2)
+    fail "wrong order"
+  end
+  #  ensure that that e1 occurs before e2.
+  #  page.body is the entire content of the page as a string.
+  #  fail "Unimplemented"
+end
+
+# Make it easier to express checking or unchecking several boxes at once
+#  "When I uncheck the following ratings: PG, G, R"
+#  "When I check the following ratings: G"
+
+When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
+  rating_list.split(" ").each do |rating|
+    if (uncheck == 'un')
+      uncheck(rating)
+    else
+      check(rating)
+    end
+  end
+  # HINT: use String#split to split up the rating_list, then
+  #   iterate over the ratings and reuse the "When I check..." or
+  #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+  # fail "Unimplemented"
+end
+
+
+Then /I should see all the movies/ do
+  if Movie.all.count == 10
+  else
+    fail "wrong number of movies"
+  end
+  # Make sure that all the movies in the app are visible in the table
+  # fail "Unimplemented"
+end
